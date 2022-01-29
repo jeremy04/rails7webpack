c972c58b1a3fdb8fb8ad9943bc6f11d7c8518a89
Jeremy Lipson <jeremy.lipson@arcadiasolutions.com>
2022-01-29 11:53:41 -0500

    added react, babel and nvmrc

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const App = () => {
+  return (<div className="alert alert-primary" role="alert">Hello, Rails 7 112!!</div>)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('app')
+  ReactDOM.render(<App />, rootEl)
+})
